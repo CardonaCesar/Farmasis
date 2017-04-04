084e57e8139cc8c328088d49a109f71f124a3c8e
CardonaCesar <isw.cesar@gmail.com>
2017-04-03 20:53:26 -0500

    Menus agregados

--- Farmasis/Form1.Designer.cs
+++ Farmasis/Form1.Designer.cs
@@ -28,20 +28,252 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tcFarmasis = new MetroFramework.Controls.MetroTabControl();
+            this.lblUsuario = new MetroFramework.Controls.MetroLabel();
+            this.tpInicio = new MetroFramework.Controls.MetroTabPage();
+            this.tpProductos = new MetroFramework.Controls.MetroTabPage();
+            this.tpProveedores = new MetroFramework.Controls.MetroTabPage();
+            this.tpClientes = new MetroFramework.Controls.MetroTabPage();
+            this.tpMedicos = new MetroFramework.Controls.MetroTabPage();
+            this.tpMovimientosInternos = new MetroFramework.Controls.MetroTabPage();
+            this.tpCompras = new MetroFramework.Controls.MetroTabPage();
+            this.tpVentas = new MetroFramework.Controls.MetroTabPage();
+            this.tpHospitalizacion = new MetroFramework.Controls.MetroTabPage();
+            this.tpUtilerias = new MetroFramework.Controls.MetroTabPage();
+            this.tpAyuda = new MetroFramework.Controls.MetroTabPage();
+            this.metroTabPage1 = new MetroFramework.Controls.MetroTabPage();
+            this.tcFarmasis.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tcFarmasis
+            // 
+            this.tcFarmasis.Controls.Add(this.tpInicio);
+            this.tcFarmasis.Controls.Add(this.tpProductos);
+            this.tcFarmasis.Controls.Add(this.tpProveedores);
+            this.tcFarmasis.Controls.Add(this.tpClientes);
+            this.tcFarmasis.Controls.Add(this.tpMedicos);
+            this.tcFarmasis.Controls.Add(this.tpMovimientosInternos);
+            this.tcFarmasis.Controls.Add(this.tpCompras);
+            this.tcFarmasis.Controls.Add(this.tpVentas);
+            this.tcFarmasis.Controls.Add(this.tpHospitalizacion);
+            this.tcFarmasis.Controls.Add(this.tpUtilerias);
+            this.tcFarmasis.Controls.Add(this.tpAyuda);
+            this.tcFarmasis.Controls.Add(this.metroTabPage1);
+            this.tcFarmasis.Location = new System.Drawing.Point(24, 64);
+            this.tcFarmasis.Name = "tcFarmasis";
+            this.tcFarmasis.SelectedIndex = 11;
+            this.tcFarmasis.Size = new System.Drawing.Size(856, 335);
+            this.tcFarmasis.TabIndex = 0;
+            this.tcFarmasis.UseSelectable = true;
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Location = new System.Drawing.Point(774, 28);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(53, 19);
+            this.lblUsuario.TabIndex = 1;
+            this.lblUsuario.Text = "Usuario";
+            // 
+            // tpInicio
+            // 
+            this.tpInicio.HorizontalScrollbarBarColor = true;
+            this.tpInicio.HorizontalScrollbarHighlightOnWheel = false;
+            this.tpInicio.HorizontalScrollbarSize = 10;
+            this.tpInicio.Location = new System.Drawing.Point(4, 38);
+            this.tpInicio.Name = "tpInicio";
+            this.tpInicio.Size = new System.Drawing.Size(816, 293);
+            this.tpInicio.TabIndex = 0;
+            this.tpInicio.Text = "Inicio";
+            this.tpInicio.VerticalScrollbarBarColor = true;
+            this.tpInicio.VerticalScrollbarHighlightOnWheel = false;
+            this.tpInicio.VerticalScrollbarSize = 10;
+            // 
+            // tpProductos
+            // 
+            this.tpProductos.HorizontalScrollbarBarColor = true;
+            this.tpProductos.HorizontalScrollbarHighlightOnWheel = false;
+            this.tpProductos.HorizontalScrollbarSize = 10;
+            this.tpProductos.Location = new System.Drawing.Point(4, 38);
+            this.tpProductos.Name = "tpProductos";
+            this.tpProductos.Size = new System.Drawing.Size(816, 293);
+            this.tpProductos.TabIndex = 1;
+            this.tpProductos.Text = "Productos";
+            this.tpProductos.VerticalScrollbarBarColor = true;
+            this.tpProductos.VerticalScrollbarHighlightOnWheel = false;
+            this.tpProductos.VerticalScrollbarSize = 10;
+            // 
+            // tpProveedores
+            // 
+            this.tpProveedores.HorizontalScrollbarBarColor = true;
+            this.tpProveedores.HorizontalScrollbarHighlightOnWheel = false;
+            this.tpProveedores.HorizontalScrollbarSize = 10;
+            this.tpProveedores.Location = new System.Drawing.Point(4, 38);
+            this.tpProveedores.Name = "tpProveedores";
+            this.tpProveedores.Size = new System.Drawing.Size(816, 293);
+            this.tpProveedores.TabIndex = 2;
+            this.tpProveedores.Text = "Proveedores";
+            this.tpProveedores.VerticalScrollbarBarColor = true;
+            this.tpProveedores.VerticalScrollbarHighlightOnWheel = false;
+            this.tpProveedores.VerticalScrollbarSize = 10;
+            // 
+            // tpClientes
+            // 
+            this.tpClientes.HorizontalScrollbarBarColor = true;
+            this.tpClientes.HorizontalScrollbarHighlightOnWheel = false;
+            this.tpClientes.HorizontalScrollbarSize = 10;
+            this.tpClientes.Location = new System.Drawing.Point(4, 38);
+            this.tpClientes.Name = "tpClientes";
+            this.tpClientes.Size = new System.Drawing.Size(816, 293);
+            this.tpClientes.TabIndex = 3;
+            this.tpClientes.Text = "Clientes";
+            this.tpClientes.VerticalScrollbarBarColor = true;
+            this.tpClientes.VerticalScrollbarHighlightOnWheel = false;
+            this.tpClientes.VerticalScrollbarSize = 10;
+            // 
+            // tpMedicos
+            // 
+            this.tpMedicos.HorizontalScrollbarBarColor = true;
+            this.tpMedicos.HorizontalScrollbarHighlightOnWheel = false;
+            this.tpMedicos.HorizontalScrollbarSize = 10;
+            this.tpMedicos.Location = new System.Drawing.Point(4, 38);
+            this.tpMedicos.Name = "tpMedicos";
+            this.tpMedicos.Size = new System.Drawing.Size(816, 293);
+            this.tpMedicos.TabIndex = 4;
+            this.tpMedicos.Text = "Medicos";
+            this.tpMedicos.VerticalScrollbarBarColor = true;
+            this.tpMedicos.VerticalScrollbarHighlightOnWheel = false;
+            this.tpMedicos.VerticalScrollbarSize = 10;
+            // 
+            // tpMovimientosInternos
+            // 
+            this.tpMovimientosInternos.HorizontalScrollbarBarColor = true;
+            this.tpMovimientosInternos.HorizontalScrollbarHighlightOnWheel = false;
+            this.tpMovimientosInternos.HorizontalScrollbarSize = 10;
+            this.tpMovimientosInternos.Location = new System.Drawing.Point(4, 38);
+            this.tpMovimientosInternos.Name = "tpMovimientosInternos";
+            this.tpMovimientosInternos.Size = new System.Drawing.Size(816, 293);
+            this.tpMovimientosInternos.TabIndex = 5;
+            this.tpMovimientosInternos.Text = "Movimientos int.";
+            this.tpMovimientosInternos.VerticalScrollbarBarColor = true;
+            this.tpMovimientosInternos.VerticalScrollbarHighlightOnWheel = false;
+            this.tpMovimientosInternos.VerticalScrollbarSize = 10;
+            // 
+            // tpCompras
+            // 
+            this.tpCompras.HorizontalScrollbarBarColor = true;
+            this.tpCompras.HorizontalScrollbarHighlightOnWheel = false;
+            this.tpCompras.HorizontalScrollbarSize = 10;
+            this.tpCompras.Location = new System.Drawing.Point(4, 38);
+            this.tpCompras.Name = "tpCompras";
+            this.tpCompras.Size = new System.Drawing.Size(816, 293);
+            this.tpCompras.TabIndex = 6;
+            this.tpCompras.Text = "Compras";
+            this.tpCompras.VerticalScrollbarBarColor = true;
+            this.tpCompras.VerticalScrollbarHighlightOnWheel = false;
+            this.tpCompras.VerticalScrollbarSize = 10;
+            // 
+            // tpVentas
+            // 
+            this.tpVentas.HorizontalScrollbarBarColor = true;
+            this.tpVentas.HorizontalScrollbarHighlightOnWheel = false;
+            this.tpVentas.HorizontalScrollbarSize = 10;
+            this.tpVentas.Location = new System.Drawing.Point(4, 38);
+            this.tpVentas.Name = "tpVentas";
+            this.tpVentas.Size = new System.Drawing.Size(816, 293);
+            this.tpVentas.TabIndex = 7;
+            this.tpVentas.Text = "Ventas";
+            this.tpVentas.VerticalScrollbarBarColor = true;
+            this.tpVentas.VerticalScrollbarHighlightOnWheel = false;
+            this.tpVentas.VerticalScrollbarSize = 10;
+            // 
+            // tpHospitalizacion
+            // 
+            this.tpHospitalizacion.HorizontalScrollbarBarColor = true;
+            this.tpHospitalizacion.HorizontalScrollbarHighlightOnWheel = false;
+            this.tpHospitalizacion.HorizontalScrollbarSize = 10;
+            this.tpHospitalizacion.Location = new System.Drawing.Point(4, 38);
+            this.tpHospitalizacion.Name = "tpHospitalizacion";
+            this.tpHospitalizacion.Size = new System.Drawing.Size(816, 293);
+            this.tpHospitalizacion.TabIndex = 8;
+            this.tpHospitalizacion.Text = "Hospitalizacion";
+            this.tpHospitalizacion.VerticalScrollbarBarColor = true;
+            this.tpHospitalizacion.VerticalScrollbarHighlightOnWheel = false;
+            this.tpHospitalizacion.VerticalScrollbarSize = 10;
+            // 
+            // tpUtilerias
+            // 
+            this.tpUtilerias.HorizontalScrollbarBarColor = true;
+            this.tpUtilerias.HorizontalScrollbarHighlightOnWheel = false;
+            this.tpUtilerias.HorizontalScrollbarSize = 10;
+            this.tpUtilerias.Location = new System.Drawing.Point(4, 38);
+            this.tpUtilerias.Name = "tpUtilerias";
+            this.tpUtilerias.Size = new System.Drawing.Size(816, 293);
+            this.tpUtilerias.TabIndex = 9;
+            this.tpUtilerias.Text = "Utilerias";
+            this.tpUtilerias.VerticalScrollbarBarColor = true;
+            this.tpUtilerias.VerticalScrollbarHighlightOnWheel = false;
+            this.tpUtilerias.VerticalScrollbarSize = 10;
+            // 
+            // tpAyuda
+            // 
+            this.tpAyuda.HorizontalScrollbarBarColor = true;
+            this.tpAyuda.HorizontalScrollbarHighlightOnWheel = false;
+            this.tpAyuda.HorizontalScrollbarSize = 10;
+            this.tpAyuda.Location = new System.Drawing.Point(4, 38);
+            this.tpAyuda.Name = "tpAyuda";
+            this.tpAyuda.Size = new System.Drawing.Size(816, 293);
+            this.tpAyuda.TabIndex = 10;
+            this.tpAyuda.Text = "Ayuda";
+            this.tpAyuda.VerticalScrollbarBarColor = true;
+            this.tpAyuda.VerticalScrollbarHighlightOnWheel = false;
+            this.tpAyuda.VerticalScrollbarSize = 10;
+            // 
+            // metroTabPage1
+            // 
+            this.metroTabPage1.HorizontalScrollbarBarColor = true;
+            this.metroTabPage1.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroTabPage1.HorizontalScrollbarSize = 10;
+            this.metroTabPage1.Location = new System.Drawing.Point(4, 38);
+            this.metroTabPage1.Name = "metroTabPage1";
+            this.metroTabPage1.Size = new System.Drawing.Size(848, 293);
+            this.metroTabPage1.TabIndex = 11;
+            this.metroTabPage1.Text = "Acerca de";
+            this.metroTabPage1.VerticalScrollbarBarColor = true;
+            this.metroTabPage1.VerticalScrollbarHighlightOnWheel = false;
+            this.metroTabPage1.VerticalScrollbarSize = 10;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(300, 300);
+            this.ClientSize = new System.Drawing.Size(895, 422);
+            this.Controls.Add(this.lblUsuario);
+            this.Controls.Add(this.tcFarmasis);
             this.Name = "Form1";
             this.Text = "Farmasis";
+            this.tcFarmasis.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private MetroFramework.Controls.MetroTabControl tcFarmasis;
+        private MetroFramework.Controls.MetroTabPage tpInicio;
+        private MetroFramework.Controls.MetroLabel lblUsuario;
+        private MetroFramework.Controls.MetroTabPage tpProductos;
+        private MetroFramework.Controls.MetroTabPage tpProveedores;
+        private MetroFramework.Controls.MetroTabPage tpClientes;
+        private MetroFramework.Controls.MetroTabPage tpMedicos;
+        private MetroFramework.Controls.MetroTabPage tpMovimientosInternos;
+        private MetroFramework.Controls.MetroTabPage tpCompras;
+        private MetroFramework.Controls.MetroTabPage tpVentas;
+        private MetroFramework.Controls.MetroTabPage tpHospitalizacion;
+        private MetroFramework.Controls.MetroTabPage tpUtilerias;
+        private MetroFramework.Controls.MetroTabPage tpAyuda;
+        private MetroFramework.Controls.MetroTabPage metroTabPage1;
     }
 }
 
